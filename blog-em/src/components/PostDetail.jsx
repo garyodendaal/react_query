721f679dc4115c7fa7ad8f323866e93d7e0ed3c4
Gary Odendaal <gary@garyo.co.za>
2022-11-02 06:55:34 +0200

    mutation practice and code quiz

--- blog-em/src/components/PostDetail.jsx
+++ blog-em/src/components/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -30,10 +30,33 @@ export function PostDetail({ post }) {
     () => fetchComments(post.id)
   );
 
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {deleteMutation.isError && (
+        <p style={{ color: "red" }}>Error deleting the post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: "purple" }}>Deleting the post...</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>Deleting the post successful</p>
+      )}
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>Error Updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: "purple" }}>Updating the post...</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>Updating the post successful</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {isLoading && <h3>Loading...</h3>}
